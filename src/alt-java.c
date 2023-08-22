aa6a0d1e7d8bb9ab3d17d36973bf3f818a3d5238
Andrew Haley <aph@redhat.com>
2023-08-22 13:49:10 +0200

    Create java launcher enabling the mitigation

---
+++ src/alt-java.c
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2023, Red Hat, Inc.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <errno.h>
+#include <libgen.h>
+#include <linux/limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/prctl.h>
+#include <unistd.h>
+
+/* Per task speculation control */
+#ifndef PR_GET_SPECULATION_CTRL
+# define PR_GET_SPECULATION_CTRL    52
+#endif
+#ifndef PR_SET_SPECULATION_CTRL
+# define PR_SET_SPECULATION_CTRL    53
+#endif
+/* Speculation control variants */
+#ifndef PR_SPEC_STORE_BYPASS
+# define PR_SPEC_STORE_BYPASS          0
+#endif
+/* Return and control values for PR_SET/GET_SPECULATION_CTRL */
+
+#ifndef PR_SPEC_NOT_AFFECTED
+# define PR_SPEC_NOT_AFFECTED          0
+#endif
+#ifndef PR_SPEC_PRCTL
+# define PR_SPEC_PRCTL                 (1UL << 0)
+#endif
+#ifndef PR_SPEC_ENABLE
+# define PR_SPEC_ENABLE                (1UL << 1)
+#endif
+#ifndef PR_SPEC_DISABLE
+# define PR_SPEC_DISABLE               (1UL << 2)
+#endif
+#ifndef PR_SPEC_FORCE_DISABLE
+# define PR_SPEC_FORCE_DISABLE         (1UL << 3)
+#endif
+#ifndef PR_SPEC_DISABLE_NOEXEC
+# define PR_SPEC_DISABLE_NOEXEC        (1UL << 4)
+#endif
+
+static void set_speculation() {
+  // PR_SPEC_DISABLE_NOEXEC doesn't surive execve, so we can't use it
+  //  if ( prctl(PR_SET_SPECULATION_CTRL,
+  //           PR_SPEC_STORE_BYPASS,
+  //           PR_SPEC_DISABLE_NOEXEC, 0, 0) == 0 ) {
+  //  return;
+  // }
+  prctl(PR_SET_SPECULATION_CTRL, PR_SPEC_STORE_BYPASS, PR_SPEC_DISABLE, 0, 0);
+}
+
+int main(int argc __attribute__((unused)), char **argv) {
+  set_speculation();
+
+  char our_name[PATH_MAX], java_name[PATH_MAX];
+  ssize_t len = readlink("/proc/self/exe", our_name, PATH_MAX - 1);
+  if (len < 0) {
+    perror("I can't find myself");
+    exit(2);
+  }
+
+  our_name[len] = '\0'; // readlink(2) doesn't append a null byte
+  char *path = dirname(our_name);
+  strncpy(java_name, path, PATH_MAX - 1);
+
+  size_t remaining_bytes = PATH_MAX - strlen(path) - 1;
+  strncat(java_name, "/java", remaining_bytes);
+
+  execv(java_name, argv);
+  fprintf(stderr, "%s failed to launch: %s\n", java_name, strerror(errno));
+
+  exit(1);
+}
